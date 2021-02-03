ff6c3c8ceede19a161c76019be85350f8e0af407
rice <nori_nori_0901@yahoo.co.jp>
2021-02-03 20:07:01 +0900

    add cfn template 20210203

---
+++ cloudformation/20210203/cfn_deploy.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+CHANGESET_OPTION="--no-execute-changeset"
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  CHANGESET_OPTION=""
+fi
+
+# 実行するcnfテンプレートの指定と作成するstack名の定義
+CNF_TEMPLATE=/home/ec2-user/environment/CloudFormation/prod/00-index_packaged.yml
+CNF_PACKAGE_TEMPLATE=/home/ec2-user/environment/CloudFormation/prod/00-index.yml
+CNF_STACK_NAME=ProdMyBlog
+
+# s3にアップロードしテンプレートパスを変換する
+aws cloudformation package --template-file ${CNF_PACKAGE_TEMPLATE} --s3-bucket cloudformation4myblog-riceincom --output-template-file ${CNF_TEMPLATE}
+aws cloudformation deploy --stack-name ${CNF_STACK_NAME} --template-file ${CNF_TEMPLATE} ${CHANGESET_OPTION} --capabilities CAPABILITY_NAMED_IAM
+#--parameter-overrides \
+#NameTagPrefix=prd \
+#VPCCIDR=10.100
