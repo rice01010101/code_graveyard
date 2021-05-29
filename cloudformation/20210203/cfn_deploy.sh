17dbb63b8d2e4fab72ae3268c5097a1ad440c2ac
rice <nori_nori_0901@yahoo.co.jp>
2021-05-29 19:18:30 +0900

    a

--- cloudformation/20210203/cfn_deploy.sh
+++ cloudformation/20210203/cfn_deploy.sh
@@ -10,10 +10,11 @@ fi
 # 実行するcnfテンプレートの指定と作成するstack名の定義
 CNF_TEMPLATE=/home/ec2-user/environment/CloudFormation/prod/00-index_packaged.yml
 CNF_PACKAGE_TEMPLATE=/home/ec2-user/environment/CloudFormation/prod/00-index.yml
-CNF_STACK_NAME=ProdMyBlog
+CNF_STACK_NAME=REPLACE_YOUR_STACK_NAME
+CFN_S3_BUCKETNAME4TEMPLATE=REPLACE_YOUR_S3_BUCKET_NAME
 
 # s3にアップロードしテンプレートパスを変換する
-aws cloudformation package --template-file ${CNF_PACKAGE_TEMPLATE} --s3-bucket cloudformation4myblog-riceincom --output-template-file ${CNF_TEMPLATE}
+aws cloudformation package --template-file ${CNF_PACKAGE_TEMPLATE} --s3-bucket ${CFN_S3_BUCKETNAME4TEMPLATE} --output-template-file ${CNF_TEMPLATE}
 aws cloudformation deploy --stack-name ${CNF_STACK_NAME} --template-file ${CNF_TEMPLATE} ${CHANGESET_OPTION} --capabilities CAPABILITY_NAMED_IAM
 #--parameter-overrides \
 #NameTagPrefix=prd \
